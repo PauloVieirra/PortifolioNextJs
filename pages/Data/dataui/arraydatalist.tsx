7c82465a2db2797f82e5cf6b6606c180323a3fac
Paulo Vieira <vieirajjr@gmail.com.com>
2024-02-19 16:30:53 -0300

    testetype

--- pages/Data/dataui/arraydatalist.tsx
+++ pages/Data/dataui/arraydatalist.tsx
@@ -14,7 +14,7 @@ const Uis = () => {
       icon: '../../images/imgUis/busine.png',
       skills: 'Auto Layout, Empty Spaces, Cards, Color and Text Hierarchy, Navigation, Components, and Variables.',
       embedCode: '<iframe width="100%" height="800px"  src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FDTNZMU21TjsG73EpTJMNKi%2FVgents-Solu%25C3%25A7oes%3Fpage-id%3D1%253A5%26type%3Ddesign%26node-id%3D4-60%26viewport%3D-66%252C176%252C0.15%26t%3D09hiPHlM1AaZuGf1-1%26scaling%3Dmin-zoom%26starting-point-node-id%3D4%253A60%26mode%3Ddesign" allowfullscreen></iframe>',
-      rating: '  7.6',
+      rating:  7.6,
     },
     { 
   
@@ -23,7 +23,7 @@ const Uis = () => {
       icon: '../../images/imgUis/streaming.png',
       skills: ' Information Design, Visual Hierarchy, Color Palette and Color Theory, Usability Testing,Visual Storytelling',
       embedCode:'<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="800px" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fv1ey3IIMcpssM4tqAQwFe0%2FUntitled%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D49-115%26viewport%3D1150%252C854%252C0.71%26t%3DRgDL3ziYKId5k6sy-1%26scaling%3Dmin-zoom%26mode%3Ddesign" allowfullscreen></iframe>',
-      rating: '7.6',
+      rating: 7.6,
     },
     {   
       
@@ -32,7 +32,7 @@ const Uis = () => {
         icon: '../../images/imgUis/music.png',
         skills: '/imagem1.jpg',
         embedCode:'123456',
-        rating: '7.6',
+        rating: 7.6,
       },
       {
        
@@ -41,7 +41,7 @@ const Uis = () => {
         icon: '../../images/imgUis/travel.png',
         skills: '/imagem2.jpg',
         embedCode:'<iframe  style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="800px" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FXT4BGk4CaQSol8livrR0Yg%2FTurism%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D2-5%26viewport%3D31%252C699%252C1%26t%3Doiy6zSVj2bU4gNiI-1%26scaling%3Dmin-zoom%26mode%3Ddesign" allowfullscreen></iframe>',
-        rating: '7.6',
+        rating: 7.6,
       },
       { 
       
@@ -50,7 +50,7 @@ const Uis = () => {
         icon: '../../images/imgUis/dahsboard.png',
         skills: './images/wireframe.png',
         embedCode:'<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="800px" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F9ZNf1evZy2dP0eeSaTkCat%2FUntitled%3Fpage-id%3D0%253A1%26node-id%3D6-4%26mode%3Ddesign%26t%3DxPv4SWCRNLKco7Nq-1" allowfullscreen></iframe>',
-        rating: '7.6',
+        rating: 7.6,
     },
       { 
        
@@ -59,7 +59,7 @@ const Uis = () => {
         icon: '../../images/imgUis/basquete.png',
         skills: './images/wireframe.png',
         embedCode:'123456',
-        rating: '7.6',
+        rating: 7.6,
     },
      
   ];
